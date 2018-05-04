93aee66629aaeafe901a43d52418851213efc0c6
cgoez <goez2001@gmail.com>
2018-05-04 10:29:46 -0700

    actionsRouter POST check for required fields

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -7,6 +7,7 @@ const router = express.Router();
 const actionModel = require("../data/helpers/actionModel");
 
 // GET
+// test ok
 router.get("/", (req, res) => {
   actionModel
     .get()
@@ -21,6 +22,7 @@ router.get("/", (req, res) => {
 });
 
 // GET :id
+// test ok
 router.get("/:id", (req, res) => {
   const { id } = req.params;
 
@@ -41,16 +43,22 @@ router.get("/:id", (req, res) => {
 router.post("/", (req, res) => {
   const action = req.body;
 
-  actionModel
-    .insert(action)
-    .then(response => {
-      res.status(200).json(response);
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: "There was an error posting the action."
+  if (action.project_id && action.description && action.notes) {
+    actionModel
+      .insert(action)
+      .then(response => {
+        res.status(201).json(response);
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error posting the action."
+        });
       });
+  } else {
+    res.status(400).json({
+      error: "Please provide a project id, description, AND notes."
     });
+  }
 });
 
 // PUT ; update()
